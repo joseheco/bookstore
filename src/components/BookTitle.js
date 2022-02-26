a68c55876a3272c06b42a9dbfb002c39bb3362c9
Jose Herrera <jferhc@gmail.com>
2022-02-26 18:40:56 -0500

    add with components

---
+++ src/components/BookTitle.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function BookTitle() {
+  return (
+    <div className="title">
+      <p>Mystery</p>
+      <h2>Harry Potter</h2>
+      <h3>J. K. Rowling</h3>
+      <div className="Mystery">
+        <button type="button">Comments</button>
+        <button type="button">Remove</button>
+        <button type="button">Edit</button>
+      </div>
+    </div>
+  );
+}
